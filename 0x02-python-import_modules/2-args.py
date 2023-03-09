34a02be0b2a6a3165125c81dadc304aeef32c3fc
Elias <eliaseshetu28@gmail.com>
2023-03-09 20:34:42 +0300

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 __name__="__main__"
 import sys
-arg = sys.argv #list
-len = len(arg)
-x = 0
-if len == 0:
-        print("{} arguments.".format(len))
-elif len == 1:
-        print("{} argument:".format(len))
-        print("{}: {}".format(len,arg[0]))
+arg = sys.argv 
+len = len(arg) 
+
+if len == 1:
+    print("{} arguments.".format(len-1))
+elif len == 2:
+    print("{} argument:".format(len-1))
+    print("{}: {}".format(len-1,arg[1]))
 else:
-    print("{}: arguments:".format(len))
-    for i in arg:
-        print("{}: {}".format(x+1, i))
+    int("{} arguments:".format(len-1))
+    for i in range(1,len):
+        print("{:d}: {}".format(i, arg[i]))
