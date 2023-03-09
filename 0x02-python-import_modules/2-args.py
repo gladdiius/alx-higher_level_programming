c8231348edb7ff42ea90467b8a4cf5b6137dab5a
Elias <eliaseshetu28@gmail.com>
2023-03-09 19:22:31 +0300

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+import sys
+arg = sys.argv
+len=len(arg)
+x = 0
+if len == 0:
+        print("{} arguments.".format(arg))
+elif len == 1:
+        print("{}: argument.".format(arg))
+        print("{} .".format(arg[0]))
+else:
+        print("{}: arguments.".format(arg))
+        for i in arg:
+            print("{}: {}".format(x+1, i))
