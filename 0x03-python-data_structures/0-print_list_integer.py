204787b8f12519606cfacf09687e0295ea3c12dc
Elias <eliaseshetu28@gmail.com>
2023-03-12 14:27:19 +0300

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-__name__="__main__"
-def print_list_integer(my_list=[]):
-    for lists in my_list:
-        print("{}".format(lists))
+__name__ = "__main__"
+def print_list_integer(my_list = []):
+    for i in my_list:
+        print("{}".format(i))
