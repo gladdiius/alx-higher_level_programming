55c4f77552d759d44b1ea83f7603625b4ee85092
Elias <eliaseshetu28@gmail.com>
2023-03-13 22:18:30 +0300

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+__name__="__main__"
+
+
+def divisible_by_2(my_list=[]):
+    multiple =[]
+    for i in my_list[i]:
+        if i % 2 == 0: 
+            multiple.append(True) 
+        else: 
+            multiple.append(False)
+    return multiple
