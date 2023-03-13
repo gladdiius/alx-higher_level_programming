4e4d7a1779ed800b677e6763e9994463fbca232c
Elias <eliaseshetu28@gmail.com>
2023-03-13 20:20:32 +0300

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ __name__ = "__main__"
 
 def multiple_returns(sentence):
         if len(sentence) == 0:
-            return None
+            return (0, None)
         else:
             return (len(sentence), sentence[0])
 
