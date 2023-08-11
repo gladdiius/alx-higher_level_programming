0ef6e25337871134d21e0224e46627354f426977
elias_eshetu <eliaseshetu28@gmail.com>
2023-08-11 21:22:14 +0300

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-__name__ == "__main__":
-from calculator_1 import add
-a = 1
-b = 2
-print('{} + {} = {}'.format(a, b, add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print('{} + {} = {}'.format(a, b, add(a, b)))
 
