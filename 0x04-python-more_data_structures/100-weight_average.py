14313eb0e794313d227fa6b1fff7a0569332bfc7
elias_eshetu <eliaseshetu28@gmail.com>
2023-08-18 21:55:10 +0300

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     sum1 = sum2 = 0
+    if my_list == None:
+        return 0
     for i in my_list:
         sum1 += i[0] * i[1]
         sum2 += i[1]
