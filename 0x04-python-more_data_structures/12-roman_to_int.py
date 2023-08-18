f1fab0246a1b2fc5de62dc50b7ec4a2ad79ec928
elias_eshetu <eliaseshetu28@gmail.com>
2023-08-18 21:49:07 +0300

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#usr/bin/python3
 def roman_to_int(roman_string):
-    if not (isinstance(roman_string, str)):
-        return
+    if not (isinstance(roman_string, str)) or not roman_string:
+        return 0
     roman = {
         'I': 1,
         'V': 5,
@@ -11,7 +11,7 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
             }
-    x = 0
+    num = 0
     len1 = len(roman_string) - 1
     flage = 0
     flage2 = 0
@@ -19,15 +19,17 @@ def roman_to_int(roman_string):
         if (len1 + 1) >= 2:
             if roman_string[-1] != 'I':
                 roman['I'] = -1
-            if roman_string[0] == 'X' and not flage:
-                roman['X'] = -10
-                flage = 1
+            if roman_string[0] == 'X':
+                if roman_string[:2] != 'XX' and not flage:
+                    roman['X'] = -10
+                    flage = 1
             if roman_string[0] == 'C' and not flage2:
-                roman['C'] = -100
-                flage2 = 1
-        x += roman[i]
+                if roman_string[1] not in ['I', 'V', 'X', 'L']:
+                    roman['C'] = -100
+                    flage2 = 1
+        num += roman[i]
         if flage:
             roman['X'] = 10
         if flage2:
             roman['C'] = 100
-    return x
+    return num
