d5b31cdcc7e9535baf419202baee67a38cd11910
elias_eshetu <eliaseshetu28@gmail.com>
2023-08-20 18:26:39 +0300

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-char = 122
 for i in range(26):
-    print(chr(char) if i % 2 == 0 else chr(char-32), end="")
-    char -= 1
+    print(chr(122 - i) if i % 2 == 0 else chr(90 - i), end="")
