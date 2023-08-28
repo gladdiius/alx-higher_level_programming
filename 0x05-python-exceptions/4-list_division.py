28d10d24499f16904205e58504cbcde2138bb33c
elias_eshetu <eliaseshetu28@gmail.com>
2023-08-28 19:19:28 +0300

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,7 +9,7 @@ def list_division(my_list_1, my_list_2, list_length):
             new_list.append(result)
             count += 1
         except ZeroDivisionError:
-            print("divition by 0")
+            print("division by 0")
             new_list.append(0)
             continue
         except IndexError:
