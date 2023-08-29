a9a18836c5fe19209fa65e54abeb21b75dc1a729
elias_eshetu <eliaseshetu28@gmail.com>
2023-08-29 21:20:36 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a square."""
+
+
+class Square:
+    """An empty class for Square.
+        
+        Args:
+            size (int): width of square.
+    """
+    def __init__(self, size):
+        self.__size = size
