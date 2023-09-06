8d220f5976a0fc04f4cef52bd130447e467e26f9
elias_eshetu <eliaseshetu28@gmail.com>
2023-09-06 21:42:11 +0300

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""define an attribute"""
+"""define an attribute."""
 
 
 class LockedClass(object):
+    """lockes the name of the attribute."""
     __slots__ = ['first_name']
