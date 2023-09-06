2480c746b936a90fa79da26e09aaedecadd28615
elias_eshetu <eliaseshetu28@gmail.com>
2023-09-06 21:23:30 +0300

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    """returns bestschool """
-    return 'BestSchool'
+def magic_string(my_list=[]):    
+    my_list.append("BestSchool")    
+    return ', '.join(my_list)
