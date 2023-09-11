30cee4b6638abd46378560f4312ff83ca3e6b939
elias_eshetu <eliaseshetu28@gmail.com>
2023-09-11 16:59:52 +0300

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function for defination for lookup"""
+
+
+def MyList(list):
+    """returns the available attributes and methods of an object"""
+
+    def print_sorted(self):
+        """print sorted list."""
+        print(sorted(self))
