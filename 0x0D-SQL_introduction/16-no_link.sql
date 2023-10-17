068a2e95218e49403812f6284874d3d462e015d0
elias_eshetu <eliaseshetu28@gmail.com>
2023-10-17 21:23:21 +0300

    16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,5 @@
--- Computes the average score of all records in the table second_table in my MySQL server.
-SELECT AVG(`score`) AS `average`
-FROM `second_table`;
+-- quaries two columens and order them by the numeric value in desending order
+SELECT `score`, `name` 
+FROM `second_table` 
+WHERE `name` IS NOT 
+NULL ORDER BY `score` DESC;
