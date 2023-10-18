5c9b34b7f784a6857ee333b396aa29214cf71a58
elias_eshetu <eliaseshetu28@gmail.com>
2023-10-18 22:14:03 +0300

    101-not_a_comedy.sql

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+-- a script that lists all shows without the genre 
+-- Comedy in the database hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id AND tv_genres.name = 'Comedy'
+WHERE tv_genres.name IS NULL
+ORDER BY tv_shows.title ASC;
