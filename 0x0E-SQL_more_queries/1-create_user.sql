8a88d48f4547afb8b7b2731b3a85eba2e4dda1cd
elias_eshetu <eliaseshetu28@gmail.com>
2023-10-18 19:09:31 +0300

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Create user_0d_1 if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges to <link>user_0d_1</link>
+-- Grant all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- Flush privileges to apply changes
