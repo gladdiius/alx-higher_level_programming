36756ebb624481f9539cf774474cd84326c63255
elias_eshetu <eliaseshetu28@gmail.com>
2023-11-30 21:46:05 +0300

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Use curl to send a request and display the size of the body in bytes
-response_body_size=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r\n')
+response_body_size=$('curl -sI "$1" | grep -i Content-Length')
 echo "$response_body_size"
 
