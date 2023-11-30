4a6fff712836df4c53cb6a0bf3cbea7cd7f3941e
elias_eshetu <eliaseshetu28@gmail.com>
2023-11-30 22:33:50 +0300

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-# Send a GET request to the URL and display the body of the response for status code 200
-response=$(curl -s -o /dev/null -w "%{http_code}" $1)
+# test
+url=$1
+response=$(curl -s -o /dev/null -w "%{http_code}" $url)
+
 if [ $response -eq 200 ]; then
-  curl -s $1
+    curl -s $url
+else
+    echo "Non-200 status code: $response"
 fi
