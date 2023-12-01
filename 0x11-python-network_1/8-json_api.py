8033bed8183cb5adeefe8d91e599fa5d8726d981
elias_eshetu <eliaseshetu28@gmail.com>
2023-12-01 20:48:45 +0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,22 +2,26 @@
 import requests
 import sys
 
-# Setting the default value for q if no argument is given
+# Set the URL for the POST request
+url = "http://0.0.0.0:5000/search_user"
+
+# Set the parameter 'q' to the provided letter or an empty string if not provided
 q = sys.argv[1] if len(sys.argv) > 1 else ""
 
 # Sending a POST request to the specified URL using requests
-response = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+response = requests.post(url, data={'q': q})
 
 try:
-    # Parsing the response as JSON
-    result = response.json()
+    # Try to parse the response as JSON
+    json_data = response.json()
 
-    # Checking if the JSON is properly formatted and not empty
-    if result:
-        print(f"[{result['id']}] {result['name']}")
+    # Check if the JSON is properly formatted and not empty
+    if json_data:
+        print(f"[{json_data['id']}] {json_data['name']}")
     else:
         print("No result")
 
 except ValueError:
-    # Handling invalid JSON
+    # Handle the case where the response is not a valid JSON
     print("Not a valid JSON")
+
