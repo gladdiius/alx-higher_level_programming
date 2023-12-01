3bea295e6d3639348ba115c2e996929378eb0c21
elias_eshetu <eliaseshetu28@gmail.com>
2023-12-01 20:20:29 +0300

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,14 +4,15 @@ import urllib.request
 import sys
 
 if len(sys.argv) != 3:
-    print("Usage: python script.py <URL>")
+    print("Usage: python script.py <URL> <email>")
     sys.exit(1)
+
 url = sys.argv[1]
 email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email})
-data = data.encode('utf-8')
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
 
-req = urllib.request.Request(url, data=data, method='POST')
-with urllib.request.urlopen(req) as response:
-    print(f'Your email is: {response.read().decode('utf-8')}')
+with urllib.request.urlopen(url, data=data) as response:
+    # Reading and decoding the body of the response
+    body = response.read().decode('utf-8')
+    print(body)
