7988eb388e1d6e448cdfb26b684dbdbe931ed74f
elias_eshetu <eliaseshetu28@gmail.com>
2023-12-01 20:41:03 +0300

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 import requests
 import sys
-
 if len(sys.argv) != 2:
-    print("Usage: ./7-error_code.py <URL>")
     sys.exit(1)
-
 url = sys.argv[1]
-
 response = requests.get(url)
-
 if response.status_code >= 400:
     print(f"Error code: {response.status_code}")
