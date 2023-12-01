3436d47b9a69e3fbe9450eafcbf84d737cc05e38
elias_eshetu <eliaseshetu28@gmail.com>
2023-12-01 19:39:24 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
+def fetch_status(url):
+    """
+    Fetches the status from the given URL using the urllib package.
 
-try:
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        print(f"Body response:\n\t- type: {type(body)}\n\t- content: {body.decode('utf-8')}")
-except urllib.error.HTTPError as e:
-    print(f"Error code: {e.code}")
-except urllib.error.URLError as e:
-    print(f"Error: {e.reason}")
+    :param url: The URL to fetch the status from.
+    :type url: str
+    """
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            print(f"Body response:\n\t- type: {type(body)}\n\t- content: {body.decode('utf-8')}")
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
+    except urllib.error.URLError as e:
+        print(f"Error: {e.reason}")
+
+if __name__ == "__main__":
+    # Example usage:
+    url = "https://alx-intranet.hbtn.io/status"
+    fetch_status(url)
 
