6935c446a87f3d396bec4688fc04ed345c329586
elias_eshetu <eliaseshetu28@gmail.com>
2023-12-01 20:07:50 +0300

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import urllib.request
 import sys
-
+if len(sys.argv) != 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
 url = sys.argv[1]
 
 req = urllib.request.Request(url)
