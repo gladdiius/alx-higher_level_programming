ef0ae8a6d448dcfd259f00b6bedc0247693d5b9c
elias_eshetu <eliaseshetu28@gmail.com>
2024-01-03 19:44:40 +0300

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = 18;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    try {
+      const films = JSON.parse(body).results;
+      const moviesWithWedgeAntilles = films.filter(film =>
+        film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+      );
+      console.log(moviesWithWedgeAntilles.length);
+    } catch (parseError) {
+      console.error(parseError);
+    }
+  }
+});
