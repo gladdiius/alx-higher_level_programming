cdfc81de22ffc5ee9416b3060937e0315e21f6f3
elias_eshetu <eliaseshetu28@gmail.com>
2024-01-03 19:47:43 +0300

    5-request_store.js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    fs.writeFile(filePath, body, 'utf-8', (writeError) => {
+      if (writeError) {
+        console.error(writeError);
+      } else {
+        console.log(`Webpage content saved to ${filePath}`);
+      }
+    });
+  }
+});
