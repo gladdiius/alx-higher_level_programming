a503bae2565ca3ac540aa7ee713c2394d6aff0d2
elias_eshetu <eliaseshetu28@gmail.com>
2024-01-10 21:05:27 +0300

    0-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Wait for the HTML document to be fully loaded
+
+  // Select the <header> element and update its text color to red
+  var headerElement = document.querySelector("header");
+  if (headerElement) {
+    headerElement.style.color = "#FF0000";
+  }
+});
+
